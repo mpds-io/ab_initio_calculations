fcc1d965dfbc256fc9b5c36f42a258a03b8491ee
alinzh <siberiangodness@mail.ru>
2025-01-22 17:52:44 +0000

    Update archives receiver

--- ab_initio_calculations/MPDS/receiver.py
+++ ab_initio_calculations/MPDS/receiver.py
@@ -4,18 +4,23 @@ import os
 import requests
 import io       
 import py7zr
+import shutil
 
 import time
 import requests
 from mpds_client import MPDSDataRetrieval, MPDSDataTypes
-from utils import get_props_names_mpds
+from utils import get_props_folders_map
 
 
+arch_dir = "./mpds_archives/"
 mpds_api = MPDSDataRetrieval(dtype=MPDSDataTypes.AB_INITIO, api_key='KEY')
+result_counter = {}
 
-for prop in get_props_names_mpds():
+for prop in get_props_folders_map().keys():
+    cnt = 0
     try:
         entrys = mpds_api.get_data({'props': prop}, fields={})
+        len(entrys)
     except:
         time.sleep(2)
         continue
@@ -26,13 +31,32 @@ for prop in get_props_names_mpds():
 
             response = requests.get(archive_url)
             if response.status_code == 200:
+                curr_folder = arch_dir + prop + '/' + os.path.basename(archive_url)[:-3]
                 with py7zr.SevenZipFile(io.BytesIO(response.content), mode='r') as archive:
-                    archive.extractall(path='data/'+prop+'/'+os.path.basename(archive_url)[:-3])  
+                    archive.extractall(path=curr_folder)  
                 print(f"The archive {archive_url} is opening successfully")
+                
+                if os.path.exists(curr_folder + '/' + get_props_folders_map()[prop]):
+                    cnt += 1    
+                    shutil.rmtree(curr_folder)
+                    os.makedirs(arch_dir + prop + '/true/', exist_ok=True)
+                    with open(arch_dir + prop + '/true/' + os.path.basename(archive_url), 'wb') as f:
+                        f.write(response.content)
+                else:
+                    shutil.rmtree(curr_folder)
+                    os.makedirs(arch_dir + prop + '/false/', exist_ok=True)
+                    with open(arch_dir + prop + '/false/' + os.path.basename(archive_url), 'wb') as f:
+                        f.write(response.content)
+                
             elif response.status_code == 400:
                 break
             else:
                 print(f"Failed to load archive {archive_url}. Status:{response.status_code}")
+        result_counter[prop] = {"n_mpds_api": len(entrys), "n_real": cnt}
+        print('Result for current iteration: ', result_counter[prop])
     except Exception as e:
         print(f"Error processing property{prop}: {e}")
+        
+print('Result: ', result_counter)
+
         
--- ab_initio_calculations/MPDS/utils.py
+++ ab_initio_calculations/MPDS/utils.py
@@ -1,5 +1,19 @@
 import requests
+from math import log
+import numpy as np
 
+    
+def get_props_folders_map() -> dict:
+    mapping = {"electrical conductivity": "TRANSPORT/SIGMA.DAT", "Seebeck coefficient": "TRANSPORT/SEEBECK.DAT",
+        "enthalpy of formation": "HFORM", "vibrational spectra": "PHONON", "infrared spectra":
+        "PHONON", "Raman spectra": "PHONON", "heat capacity at constant pressure": "PHONON",
+        "isothermal bulk modulus": "ELASTIC", "poisson ratio": "ELASTIC", "effective charge": "STRUCT",
+        "energy gap for direct transition": "STRUCT", "energy gap for indirect transition": "STRUCT",
+        "energy gap": "STRUCT", "magnetic moment": "STRUCT"
+        }
+    return mapping
+
+        
 def get_props_names_mpds() -> list:
     """
     Get all avalible properties names in MPDS database
@@ -8,4 +22,99 @@ def get_props_names_mpds() -> list:
     res = requests.get(url).json()
     props = res['props']
     return props
-    
+
+def get_ab_initio_props_names_mpds() -> list:
+    props = ['electrical conductivity', 'Seebeck coefficient', 'enthalpy of formation',
+             'vibrational spectra', 'heat capacity at constant pressure', 'isothermal bulk modulus',
+             'poisson ratio', 'effective charge', 'energy gap for direct transition',
+             'energy gap for indirect transition', 'energy gap', 'magnetic moment', 'infrared spectra',
+             'Raman spectra']
+    return props
+
+def assert_conforming_input(content):
+    return  'PBE0' in content \
+        and 'XLGRID' in content \
+        and 'TOLLDENS\n8' in content \
+        and 'TOLLGRID\n16' in content \
+        and 'TOLDEE\n9' in content
+
+
+def get_raw_input_type(string):
+    if 'MOLECULE' in string:
+        return 'ISLD_ATOM'
+    elif 'FREQCALC' in string:
+        return 'PHONON'
+    elif 'ELASTCON' in string or 'ELAPIEZO' in string:
+        return 'ELASTIC'
+    elif 'OPTGEOM' in string:
+        return 'STRUCT'
+    else:
+        return None
+
+
+def get_raw_output_type(parser):
+    if parser.info['periodicity'] == 0x5:
+        return 'ISLD_ATOM'
+    elif parser.phonons['modes']:
+        return 'PHONON'
+    elif parser.elastic.get('K'):
+        return 'ELASTIC'
+    elif parser.tresholds:
+        return 'STRUCT'
+    else:
+        return None
+
+
+def get_input_precision(string):
+    try:
+        tol = tuple([ -int(x) for x in string.split('TOLINTEG')[-1].strip().split("\n")[0].split() ])
+    except:
+        tol = (-6, -6, -6, -6, -12) # default for CRYSTAL09-17
+    try:
+        kset = int(string.split('SHRINK')[-1].strip().split()[0])
+    except:
+        kset = None # molecule or isolated atom
+    kset = tuple([kset] * 3)
+    return tol, kset
+
+
+def get_input_spin(string):
+    spin = string.split('SPINLOCK')[-1].strip().split("\n")[0].strip().split()
+    #assert int(spin[1]) > 50
+    return int(spin[0])
+
+
+def get_basis_fingerprint(basis_set):
+
+    multipliers = {'S': 10, 'SP': 100, 'P': 1000, 'D': 10000, 'F': 10000, 'G': 10000, 'H': 10000}
+    bs_fgpt = {}
+
+    for el in basis_set:
+        bs_repr = []
+        for channel in basis_set[el]:
+            bs_repr.append([])
+            for coeffs in channel[1:]:
+                bs_repr[-1].append(sum([round(coeff, 1) * multipliers[channel[0]] for coeff in coeffs]))
+            bs_repr[-1] = sum(bs_repr[-1])
+        if sum(bs_repr) == 0:
+            bs_repr = [1] # gives fgpt = 0
+        bs_fgpt[el] = int(round(log(sum(bs_repr)) * 10000))
+
+    return tuple(sorted([(key, value) for key, value in bs_fgpt.items()], key=lambda x: x[0]))
+
+
+def ase_to_optimade(ase_obj, name_id=None):
+    result = dict(
+        id=name_id,
+        attributes={},
+        links=dict(self=None),
+        type='structures'
+    )
+    result['attributes']['immutable_id'] = name_id
+    result['attributes']['lattice_vectors'] = np.round(ase_obj.cell, 4).tolist()
+    result['attributes']['cartesian_site_positions'] = []
+    result['attributes']['species_at_sites'] = []
+    for atom in ase_obj:
+        result['attributes']['cartesian_site_positions'].append(np.round(atom.position, 4).tolist())
+        result['attributes']['species_at_sites'].append(atom.symbol)
+    return dict(data=[result])
