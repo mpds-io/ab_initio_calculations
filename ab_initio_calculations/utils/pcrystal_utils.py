10a646a44377efe12c1fba0123c55784dbe34c28
alinzh <siberiangodness@mail.ru>
2026-02-05 09:47:10 +0000

    Small fix

--- ab_initio_calculations/utils/pcrystal_utils.py
+++ ab_initio_calculations/utils/pcrystal_utils.py
@@ -6,10 +6,8 @@ from aiida_crystal_dft.io.basis import BasisFile
 from aiida_crystal_dft.io.d12 import D12
 from aiida_crystal_dft.io.f34 import Fort34
 from ase.data import chemical_symbols
-import numpy as np
 
 from ab_initio_calculations.settings import Settings
-from pycrystal import CRYSTOUT
 
 TEMPLATE_DIR = os.path.join(
     os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "conf/templates"
@@ -17,28 +15,6 @@ TEMPLATE_DIR = os.path.join(
 ELS_REPO_DIR = Settings().basis_sets_dir
 
 verbatim_basis = namedtuple("basis", field_names="content, all_electron")
-
-
-class Data_type:
-    structure = 1
-    calculation = 2
-    property = 3
-    workflow = 4
-    pattern = 5
-    user_input = 6
-    
-def get_avg_charges(ase_obj):
-    """
-    Get an average Mulliken charge for each chemical element in a crystal
-    """
-    at_type_chgs = {}
-    for atom in ase_obj:
-        at_type_chgs.setdefault(atom.symbol, []).append(atom.charge)
-
-    if sum([sum(at_type_chgs[at_type]) for at_type in at_type_chgs]) == 0.0:
-        return None
-
-    return {at_type: np.average(at_type_chgs[at_type]) for at_type in at_type_chgs}
 
 
 def get_basis_sets(repo_dir=ELS_REPO_DIR):
@@ -124,71 +100,21 @@ class Pcrystal_setup:
         return str(f34_input.from_ase(self.ase_obj))
 
     def get_input_setup(self, label):
-        params = (
-            self.custom_template["default"]["crystal"]
-            if self.custom_template
-            else Pcrystal_setup.calc_setup["default"]["crystal"]
+        return str(
+            get_input(
+                (
+                    self.custom_template["default"]["crystal"]
+                    if self.custom_template
+                    else Pcrystal_setup.calc_setup["default"]["crystal"]
+                ),
+                self.els,
+                Pcrystal_setup.els_repo,
+                label,
+            )
         )
 
-        return str(get_input(params, self.els, Pcrystal_setup.els_repo, label))
 
-
-    @staticmethod
-    def parse(resource):
-        if not CRYSTOUT.acceptable(resource):
-            return False
-
-        result = CRYSTOUT(resource)
-        output = {"content": {}}
-
-        if result.info["optgeom"]:
-            # output['content'] = ase_serialize(result.info['structures'][-1])
-            output["type"] = Data_type.structure
-
-        # immediately detect a domain-specific error and exit
-        if result.info["finished"] != 2:
-            try:
-                with open(os.path.join(os.path.dirname(resource), "fort.87"), "r") as f:
-                    errmsg = f.read()
-            except IOError:
-                errmsg = "Sorry, an engine has crashed"
-
-            return {"content": {"error": errmsg, "correctly_finalized": False}}
-
-        # TODO
-        # Below is just a quick example
-        # this should be more systematic
-
-        conductor, band_gap = "no data", "no data"
-        try:
-            bands_data = result.info["conduction"][-1]
-        except Exception:
-            bands_data = {}
-        if bands_data.get("state") == "CONDUCTING":
-            conductor, band_gap = True, None
-        elif bands_data.get("state") == "INSULATING":
-            conductor, band_gap = False, f"{bands_data['band_gap']:.1f}"
-        try:
-            charges = get_avg_charges(result.info["structures"][-1])
-            charges = {el: f"{val:.2f}" for el, val in charges.items()}
-        except Exception:
-            charges = None
-
-        output["content"] = {
-            #"total_energy": result.info["energy"],
-            #"total_energy_units": "eV",
-            "conductor": conductor,
-            "band_gap": band_gap,
-            "band_gap_units": "eV",
-            "charges": charges,
-            #'magmoms': magmoms,
-            "n_electrons": result.info["n_electrons"],
-            "correctly_finalized": result.info["finished"] == 2,
-        }
-        return output
-
-
-def convert_to_pcrystal_input(dir: str, atoms_obj: list[ase.Atoms], entry: str = None, optimise: bool = False) -> str:
+def convert_to_pcrystal_input(dir: str, atoms_obj: list[ase.Atoms], entry: str = None) -> str:
     """Convert structures from ase.Atoms to Pcrystal input format (d12, fort.34)"""
     el_high_tolinteg = ["Ta", "Se", "P"]
 
@@ -217,16 +143,6 @@ def convert_to_pcrystal_input(dir: str, atoms_obj: list[ase.Atoms], entry: str =
             setup.calc_setup["default"]["crystal"]["scf"]["numerical"]["MAXCYCLE"] = 1000
             setup.calc_setup["default"]["crystal"]["scf"]["numerical"]["FMIXING"] = 90
 
-        if optimise:
-            setup.calc_setup["options"]["optimize_structure"] = "optimise"
-            setup.calc_setup["default"]["crystal"]["geometry"] = {
-                "optimise": {
-                    "type": "FULLOPTG",
-                    "convergence": {
-                        "MAXCYCLE": 200,
-                    }
-                }
-            }
 
         input = setup.get_input_setup("test " + entry)
         fort34 = setup.get_input_struct()
